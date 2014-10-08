344c27e3416695041b6d44e49594fb181b76ce08
Adam Jimerson <vendion@gmail.com>
2014-10-08 19:31:51 -0400

    Adding basic layout
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- main.go
+++ main.go
@@ -1,18 +1,55 @@
 package googleipsum
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/bmizerany/pat"
+	"github.com/yosssi/gcss"
 )
 
 // define the routes during package initization.
 func init() {
 	router := pat.New()
+	// handle asset paths
+	router.Get("/css/:file", http.HandlerFunc(compileCSS))
+
 	// handle application paths
 	router.Get("/", http.HandlerFunc(rootHandler))
 	http.Handle("/", router)
+}
+
+func compileCSS(w http.ResponseWriter, r *http.Request) {
+	file := r.URL.Query().Get(":file")
+	if file == "" {
+		log.Println("did not get a filename")
+		http.Error(w, "no CSS file requested", http.StatusInternalServerError)
+	}
+
+	f := gcss.Path(file)
+	f = fmt.Sprintf("static/css/%s", f)
+	log.Printf("GCSS file: %s\n", f)
+
+	css, err := os.Open(f)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	defer func() {
+		if err := css.Close(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}()
+
+	w.Header().Set("Content-Type", "text/css")
+	_, err = gcss.Compile(w, css)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
