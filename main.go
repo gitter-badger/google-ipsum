ad4149782ed4a9c0eba8f4960126e4d3033a61d8
Adam Jimerson <vendion@gmail.com>
2014-10-08 20:29:15 -0400

    Adding in documentation
    Adding in some documentation and code comments.
    
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- main.go
+++ main.go
@@ -22,6 +22,8 @@ func init() {
 	http.Handle("/", router)
 }
 
+// compileCSS gets the CSS name from the URL, loads the appropate GCSS file,
+// then serves the client the compiled version.
 func compileCSS(w http.ResponseWriter, r *http.Request) {
 	file := r.URL.Query().Get(":file")
 	if file == "" {
@@ -29,42 +31,50 @@ func compileCSS(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "no CSS file requested", http.StatusInternalServerError)
 	}
 
+	// convert the .css extension to .gcss, and build out path to the file
 	f := gcss.Path(file)
 	f = fmt.Sprintf("static/css/%s", f)
 	log.Printf("GCSS file: %s\n", f)
 
+	// read in the GCSS file
 	css, err := os.Open(f)
 	if err != nil {
 		log.Println(err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+	// close out the file resource once done
 	defer func() {
 		if err := css.Close(); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}()
 
+	// set the content type header so browsers will know how to handle it
 	w.Header().Set("Content-Type", "text/css")
+
+	// build out the CSS and serve it to the browser
 	_, err = gcss.Compile(w, css)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+// rootHandler handles the root path, and catch all for unmatched routes
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// if the path is not the root path then it is a 404
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
-		return
 	}
 
+	// load the template files used for this page
 	page := template.Must(template.ParseFiles(
 		"static/_base.html",
 		"static/index.html",
 	))
 
+	// render the template files and serve the page
 	if err := page.Execute(w, nil); err != nil {
 		http.Error(w, "failed to load page", http.StatusInternalServerError)
-		return
 	}
 }
